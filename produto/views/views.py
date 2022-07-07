316b2c007acf984e102a63325b0c145923392ee6
Igor Dantas Quintanilha <igor.dantas@eurekalabs.com.br>
2022-07-07 19:10:48 -0300

    feat: "construção dos métodos de produto"

--- produto/views/views.py
+++ produto/views/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from models import Produtos
 
-# Create your views here.
+
+def existe():
+    return True
+def incluir():
+    return True
+def obter():
+    return True
+def salvar():
+    return True
+def excluir():
+    return True
+def baixarEstoque():
+    return True
+def subirEstoque():
+    return True
+def estoqueBaixo():
+    return True
