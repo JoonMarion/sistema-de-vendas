422c4d8c7c2beac27ab58b567d26792a49e28834
Igor Dantas Quintanilha <igor.dantas@eurekalabs.com.br>
2022-07-07 11:17:32 -0300

    fix: "ajuste no tamanho dos campos"

--- produto/models/produtos.py
+++ produto/models/produtos.py
@@ -4,9 +4,9 @@ from django.db import models
 from django.forms import DecimalField
 
 class Produtos(models.Model):
-    cod_produto = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, null=False, unique=True)
-    unidade = models.CharField(max_length=100)
-    valor_unitario = models.DecimalField(max_digits=10, decimal_places=2)
-    descricao = models.TextField()
-    estoque_minimo = models.DecimalField(max_digits=10, decimal_places=2)
-    qtde_estoque = models.DecimalField(max_digits=10, decimal_places=2)
+    cod_produto = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, null=False, unique=True, blank=False)
+    unidade = models.CharField(max_length=10, null=False, blank=False)
+    valor_unitario = models.DecimalField(max_digits=5, decimal_places=2, null=False, blank=False)
+    descricao_produto = models.TextField(max_length=30)
+    estoque_minimo = models.DecimalField(max_digits=5, decimal_places=2)
+    qtde_estoque = models.DecimalField(max_digits=5, decimal_places=2, null=False, blank=False)
