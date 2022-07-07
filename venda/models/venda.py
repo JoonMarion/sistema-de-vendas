bc953a2cfe082afe4b4db4da9430c749cce5b7a6
Igor Dantas Quintanilha <igor.dantas@eurekalabs.com.br>
2022-07-07 19:39:09 -0300

    feat: "adicionando models venda"

---
+++ venda/models/venda.py
@@ -0,0 +1,17 @@
+import uuid
+
+from cliente import models as cliente_models
+from django.db import models
+from django.forms import DecimalField
+
+class Vendas(models.Model):
+    cod_venda = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        null=False,
+        unique=True,
+        blank=False
+    )
+    cod_cliente = models.ForeignKey('cliente_models.Clientes', primary_key=True)
+    data_venda = models.DateField('Data da venda', null=False, blank=False)
